75f4e8127abe6ddd597a94ecdac1abaaf25acc22
Paul Irish <paul.irish@gmail.com>
2018-05-04 12:29:35 -0700

    audit title: 'Efficiently encode images' => 'Serve optimized images'

--- lighthouse-core/audits/byte-efficiency/uses-optimized-images.js
+++ lighthouse-core/audits/byte-efficiency/uses-optimized-images.js
@@ -22,7 +22,7 @@ class UsesOptimizedImages extends ByteEfficiencyAudit {
   static get meta() {
     return {
       name: 'uses-optimized-images',
-      description: 'Efficiently encode images',
+      description: 'Serve optimized images',
       informative: true,
       scoreDisplayMode: ByteEfficiencyAudit.SCORING_MODES.NUMERIC,
       helpText: 'Optimized images load faster and consume less cellular data. ' +
